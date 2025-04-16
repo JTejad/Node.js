3319d7d6ae4588783c53181ebd9070cfb678e4b1
JTejad <jtejada@fi.uba.ar>
2025-04-16 19:49:39 -0300

    first commit

---
+++ Funciones.js
@@ -0,0 +1,19 @@
+//Declaracion de una funcion
+
+function search(){
+    //proceso encapsulado
+}
+
+//Funciones expresadas
+
+const search = function(){
+    //proceso encapsulado
+}
+
+//Invocar funciones
+
+function subtract(){
+    return 20 - 10
+}
+
+subtract();
