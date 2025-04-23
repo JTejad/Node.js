33f0f88f05d60c0f4ef99d68627c7318a907a0bf
JTejad <jtejada@fi.uba.ar>
2025-04-23 20:20:08 -0300

    Ejercicio simple 2

---
+++ Ejercicio objetos.js
@@ -0,0 +1,34 @@
+/*1.  Crea un array con 10 objetos, donde cada objeto represente un automóvil con la 
+siguiente información: 
+○  Marca 
+○  Modelo 
+○  Año 
+○  Color 
+
+2.  Usa un método de array para recorrer la lista e imprime por consola todos los datos 
+de los automóviles cuyo año sea mayor a 2018. */
+
+//1
+const autos = [
+    { marca: 'Toyota', modelo: 'Corolla', año: 2020, color: 'Rojo'},
+    { marca: 'Honda', modelo: 'Civic', año: 2019, color: 'Azul' },
+    { marca: 'Ford', modelo: 'Focus', año: 2021, color: 'Negro'},
+    { marca: 'Volkswagen', modelo: 'Gol', año: 2006, color: 'Gris Cosmos'},
+    { marca: 'Honda', modelo: 'Fit', año: 2007, color: 'Negro'},
+    { marca: 'Ford', modelo: 'Mondeo', año: 2018, color: 'Gris'},
+    { marca: 'Hyundai', modelo: 'Tucson', año: 2020, color: 'Rojo'},
+    { marca: 'Peugeot', modelo: '206', año: 2004, color: 'Blanco'},
+    { marca: 'Volkswagen', modelo: 'Golf', año: 2004, color: 'Blanco'},
+    { marca: 'Peugeot', modelo: '208', año: 2021, color: 'Fucsia'},
+
+];
+
+//2
+
+const autosNuevos = autos.filter(auto => auto.año > 2018);
+
+autosNuevos.forEach(auto => {
+  console.log(`Marca: ${auto.marca}, Modelo: ${auto.modelo}, Año: ${auto.año}, Color: ${auto.color}`);
+});
+
+
